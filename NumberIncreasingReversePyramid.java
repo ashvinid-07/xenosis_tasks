8453f402938ad6fadb9e04360ed42c0c5d4db9ac
ashvinid-07 <146181821+ashvinid-07@users.noreply.github.com>
2024-07-21 16:51:07 +0530

    "Uploading assignment 2 "

---
+++ NumberIncreasingReversePyramid.java
@@ -0,0 +1,13 @@
+//Assignment 2: Number-increasing reverse Pyramid Pattern
+public class NumberIncreasingReversePyramid {
+    public static void main(String[] args) {
+        int rows = 6;
+    
+        for (int i = rows; i >= 1; --i) {
+          for (int j = 1; j <= i; ++j) {
+            System.out.print(j + " ");
+          }
+          System.out.println();
+        }
+      }
+}
